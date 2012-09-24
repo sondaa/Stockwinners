34b945c5d107457ed866d98913cea2eeb45e0e01
Ameen Tayyebi <ameen@tradista.com>
2012-09-24 00:58:13 -0400

    Adding feedback link

--- WebSite/Controllers/HomeController.cs
+++ WebSite/Controllers/HomeController.cs
@@ -1,14 +1,26 @@
-﻿using System;
+﻿using Stockwinners.Email;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Mail;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
+using WebSite.Helpers.Authentication;
+using WebSite.Models;
+using WebSite.Models.UI;
 
 namespace WebSite.Controllers
 {
     public class HomeController : Controller
     {
+        IEmailFactory _emailFactory;
+
+        public HomeController(IEmailFactory emailFactory)
+        {
+            _emailFactory = emailFactory;
+        }
+
         public ActionResult Index()
         {
             ViewBag.Message = "Modify this template to kick-start your ASP.NET MVC application.";
@@ -33,6 +45,46 @@ namespace WebSite.Controllers
         public ActionResult PageNotFound()
         {
             return View();
+        }
+
+        public ActionResult Feedback()
+        {
+            FeedbackModel model = new FeedbackModel();
+
+            // If the user is logged in, fill values that we know already
+            if (Request.IsAuthenticated)
+            {
+                LoggedInUserIdentity userIdentity = Authentication.GetCurrentUserIdentity();
+
+                model.Name = userIdentity.FirstName + " " + userIdentity.LastName;
+                model.EmailAddress = userIdentity.EmailAddress;
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Feedback(FeedbackModel userFeedback)
+        {
+            if (ModelState.IsValid)
+            {
+                // Create an email and shoot it to info@stockwinners.com
+                StringBuilder contentsBuilder = new StringBuilder();
+                contentsBuilder.Append("<html><head></head><body>");
+                contentsBuilder.Append(userFeedback.Name);
+                contentsBuilder.Append(" has provided invaluable feedback to us. The message is as follows: <br/><br/>");
+                contentsBuilder.Append(userFeedback.Message.Replace(Environment.NewLine, "<br/>"));
+
+                _emailFactory.CreateEmail(contentsBuilder.ToString(), "User Feedback", userFeedback.EmailAddress, new string[] { "info@stockwinners.com" }).Send();
+
+                // Clear message
+                userFeedback.Message = string.Empty;
+
+                // Let the user know that the message was successfully sent
+                userFeedback.IsSubmittedSuccessfully = true;
+            }
+
+            return this.View(userFeedback);
         }
     }
 }
---
+++ WebSite/Models/UI/FeedbackModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebSite.Models.UI
+{
+    /// <summary>
+    /// Feedback provided by the user about our services and products.
+    /// </summary>
+    public class FeedbackModel
+    {
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email Address")]
+        public string EmailAddress { get; set; }
+
+        [Required]
+        [Display(Name = "Message")]
+        public string Message { get; set; }
+
+        /// <summary>
+        /// Whether the feedback was successfully submitted
+        /// </summary>
+        public bool IsSubmittedSuccessfully { get; set; }
+    }
+}

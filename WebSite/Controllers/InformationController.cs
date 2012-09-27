e3dfa1dc9145daaa391405f714724e7fb9f47163
Ameen Tayyebi <ameen@tradista.com>
2012-09-27 16:43:00 -0400

    1) Finishing up homepage changes 2) Adding Pricing section 3) Finished adding Services section

--- WebSite/Controllers/InformationController.cs
+++ WebSite/Controllers/InformationController.cs
@@ -87,5 +87,10 @@ namespace WebSite.Controllers
         {
             return View();
         }
+
+        public ActionResult Pricing()
+        {
+            return View();
+        }
     }
 }

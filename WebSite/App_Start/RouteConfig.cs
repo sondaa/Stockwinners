5bbab27cd644b02209cd0c6d37986f60e6680e1a
Ameen Tayyebi <ameen.tayyebi@gmail.com>
2013-05-24 16:46:43 -0400

    Incorporating changes requested by the SEO company

--- WebSite/App_Start/RouteConfig.cs
+++ WebSite/App_Start/RouteConfig.cs
@@ -14,6 +14,40 @@ namespace WebSite
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            #region Search Engine Optimization Related Mappings
+
+            routes.MapRoute(
+                name: "Momentum Trading",
+                url: "momentum-trading",
+                defaults: new { controller = "SEO", action = "MomentumTrading" }
+            );
+
+            routes.MapRoute(
+                name: "Stock Market Analysis",
+                url: "stock-market-analysis",
+                defaults: new { controller = "SEO", action = "StockMarketAnalysis" }
+            );
+
+            routes.MapRoute(
+                name: "Best Daytrading Site",
+                url: "best-daytrading-site",
+                defaults: new { controller = "SEO", action = "BestDaytradingSite" }
+            );
+
+            routes.MapRoute(
+                name: "Stock Rumors",
+                url: "stock-rumors",
+                defaults: new { controller = "SEO", action = "StockRumors" }
+            );
+
+            routes.MapRoute(
+                name: "Stocks To Buy",
+                url: "stocks-to-buy",
+                defaults: new { controller = "SEO", action = "StocksToBuy" }
+            );
+
+            #endregion
+
             routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{action}/{id}",
---
+++ WebSite/Controllers/SEOController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebSite.Controllers
+{
+    public class SEOController : Controller
+    {
+        public ActionResult MomentumTrading()
+        {
+            return View();
+        }
+
+        public ActionResult StockMarketAnalysis()
+        {
+            return View();
+        }
+
+        public ActionResult BestDaytradingSite()
+        {
+            return View();
+        }
+
+        public ActionResult StockRumors()
+        {
+            return View();
+        }
+
+        public ActionResult StocksToBuy()
+        {
+            return View();
+        }
+    }
+}
